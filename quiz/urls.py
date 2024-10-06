e4048b06bbc80e64362fcf9109eff5c72f600763
Simabhagat <moirangthembhagat338@gmail.com>
2024-10-06 21:10:23 +0530

    added get  method for choices model

--- quiz/urls.py
+++ quiz/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("categories/", views.get_category_set, name="get_category_set"),
     path("<int:category_id>/quizzes/", views.get_quizzes_set, name="get_quizzes_set"),
     path("<int:quiz_id>/questions/", views.get_question_set, name="get_question_set"),
+    path("<int:question_id>/choices/", views.get_choices_set, name="get_choices_set"),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -3,19 +3,17 @@ from django.http import JsonResponse, Http404
 from .models import quizzes, questions, choices, categories
 
 # Create your views here.
-
-#Get all questions from the quiz model
-def get_question_set(request, quiz_id):
+#get all categories from the categories model
+def get_category_set(request):
     try:
-        quiz = quizzes.objects.get(pk=quiz_id) #get the quiz with the given quiz_id
-        question_set = quiz.questions_set.all() #get all questions in the quiz as querySet
-    except quizzes.DoesNotExist:
-        raise Http404("Quiz does not exist")
-    #prepare a list of questions to send as json
-    question_set = list(question_set.values("question_id", "question_text"))  #list converts it to s list of dictionaries
-    return JsonResponse({"question_set": question_set})
+        category = categories.objects.all()
+    except categories.DoesNotExist:
+        raise Http404("No categories found")
+    #prepare a list of categories to send as json
+    category_set = list(category.values("category_id", "category_name"))
+    return JsonResponse({"category_set": category_set})
 
-#get all quizzes from a category model
+#get all quizzes from the quizzes model of a category
 def get_quizzes_set(request, category_id):
     try:
         category = categories.objects.get(category_id=category_id) #get the categorywith the given category_id
@@ -26,12 +24,24 @@ def get_quizzes_set(request, category_id):
     quizzes_set = list(quizzes_set.values("quiz_id" ,"title", "pub_date"))
     return JsonResponse({"quizzes_set": quizzes_set})
 
-#get all categories from the category model
-def get_category_set(request):
+#Get all questions from the questions model of a quiz
+def get_question_set(request, quiz_id):
     try:
-        category = categories.objects.all()
-    except categories.DoesNotExist:
-        raise Http404("No categories found")
-    #prepare a list of categories to send as json
-    category_set = list(category.values("category_id", "category_name"))
-    return JsonResponse({"category_set": category_set})
+        quiz = quizzes.objects.get(pk=quiz_id) #get the quiz with the given quiz_id
+        question_set = quiz.questions_set.all() #get all questions in the quiz as querySet
+    except quizzes.DoesNotExist:
+        raise Http404("Quiz does not exist")
+    #prepare a list of questions to send as json
+    question_set = list(question_set.values("question_id", "question_text"))  #list converts it to s list of dictionaries
+    return JsonResponse({"question_set": question_set})
+
+#Get all choices from the choices model of a question
+def get_choices_set(request, question_id):
+    try:
+        question = questions.objects.get(pk=question_id) #get the question with the given question_id
+        choices_set = question.choices_set.all() #get all choices of the question as querySet
+    except questions.DoesNotExist:
+        raise Http404("Question does not exist")
+    #prepare a list of questions to send as json
+    choices_set = list(choices_set.values("choice_text", "is_correct_choice"))  #list converts it to s list of dictionaries
+    return JsonResponse({"choices_set": choices_set})
